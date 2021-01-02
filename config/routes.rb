f08a9a736820e04239c18ced60c61e9176aceb37
ayaka <ayaka.71k@gmail.com>
2021-01-02 15:55:43 +0900

    second commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'posts/index'
+  get 'users/index'
+  get 'lessons/hello'
+  get 'lessons/call'
+
+  resources :users
+  resources :posts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
 end
